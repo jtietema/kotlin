436641d2c2de837ae1a1e4780fe2f25835ceddaa
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-10-15 09:58:21 +0300

    Fix for KT-14012: Back-end (JVM) Internal error every first compilation after the source code change

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -2607,6 +2607,11 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
                 if (setter != null &&
                     !couldUseDirectAccessToProperty(propertyDescriptor, false, isDelegatedProperty, context) &&
                     !isConstOrHasJvmFieldAnnotation(propertyDescriptor)) {
+
+                    //additional check cause incremental compilation problem KT-14012, see also KotlinTypeMapper.mangleMemberNameIfRequired
+                    if (!Visibilities.isPrivate(setter.getVisibility()) ||
+                        DescriptorToSourceUtils.getContainingFile(context.getContextDescriptor()) ==
+                        DescriptorToSourceUtils.getContainingFile(setter))
                     callableSetter = typeMapper.mapToCallableMethod(setter, isSuper);
                 }
             }
